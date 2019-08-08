89f891e84de914ce33d42eddcaf4b9a1b255c37c
Ankit Verma <ankit@tomhorngaming.com>
2019-08-08 14:51:42 +0530

    Rabbit MQ and DI implementation

---
+++ MicroRabbit/MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain bus
+
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
